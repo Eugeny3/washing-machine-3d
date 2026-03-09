39f2724fb67535a229da45e51f89fc07f449d8e2
Eugeny3 <hello31323334@gmail.com>
2026-03-09 13:37:50 +0500

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 
+const repository = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+const isGitHubPagesBuild = process.env.GITHUB_ACTIONS === 'true' && repository.length > 0;
+const isUserSite = repository.toLowerCase().endsWith('.github.io');
+
 export default defineConfig({
+  base: isGitHubPagesBuild ? (isUserSite ? '/' : `/${repository}/`) : '/',
   server: {
     host: true,
     port: 5173,
